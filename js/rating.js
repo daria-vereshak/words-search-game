f4b16e044012a0565bffa6c86917e84e79aa9bcb
daria-vereshak <dariavereshak02@gmail.com>
2022-12-23 14:58:03 +0300

    fix for first use

--- js/rating.js
+++ js/rating.js
@@ -1,10 +1,11 @@
 //import './data.js';
 
 //key - name, value - score
-let USERS = JSON.parse(localStorage.getItem('users'));
-
+let USERS = {};
+if (localStorage.getItem('users')) USERS = JSON.parse(localStorage.getItem('users'));
 const current_user = JSON.parse(localStorage.getItem('user'));
 USERS[current_user['name']] = current_user['score'];
+localStorage.setItem('users', JSON.stringify(USERS));
 
 const list = document.querySelector('.list');
 
@@ -12,9 +13,9 @@ const template = document.querySelector('#list-item-template');
 
 //descenging
 function compareUsers(a, b) {
-  if (a[1] > b[1]) return -1;
-  if (a[1] == b[1]) return 0;
-  if (a[1] < b[1]) return 1;
+if (a[1] > b[1]) return -1;
+if (a[1] == b[1]) return 0;
+if (a[1] < b[1]) return 1;
 }
 
 let sortUsers = Object.entries(USERS);
@@ -22,8 +23,8 @@ let sortUsers = Object.entries(USERS);
 sortUsers.sort(compareUsers);
 
 for (let i = 0; i < sortUsers.length; i++) {
-  const item = template.content.cloneNode(true);
-  item.querySelector('.name').textContent = sortUsers[i][0];
-  item.querySelector('.score').textContent = sortUsers[i][1];
-  list.appendChild(item);
+const item = template.content.cloneNode(true);
+item.querySelector('.name').textContent = sortUsers[i][0];
+item.querySelector('.score').textContent = sortUsers[i][1];
+list.appendChild(item);
 }
